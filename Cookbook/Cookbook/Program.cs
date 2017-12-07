e6d6468e8d383d58bea5f881fe57e54bdb53d914
Edward Dupre <edwd42@gmail.com>
2017-12-07 18:11:59 -0500

    add Cookbook SQL tutorial

---
+++ Cookbook/Cookbook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cookbook
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            // This Data Source string from Server Explorer Add Connection dialog box
+            // "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\MyDox\C_sharp_projects\csharp-intermediate\WebScraperProjects\Cookbook\Cookbook\Cookbook.mdf;Integrated Security=True;Connect Timeout=30";
+        }
+    }
+}
---
+++ Cookbook/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿SELECT * FROM Recipe
+
+SELECT * FROM Ingredient
+
+SELECT a.Name FROM Ingredient a
+INNER JOIN RecipeIngredient b ON a.Id = b.IngredientId
+WHERE b.RecipeId = 1;
+
+UPDATE Recipe
+SET Name = 'Salad Deluxe'
+WHERE Id = 1;
