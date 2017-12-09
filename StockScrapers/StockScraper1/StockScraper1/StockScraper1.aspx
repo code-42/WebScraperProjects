1f7a5aa6de41d327abd4f9dd7e826dd03f4c6b6d
Edward Dupre <edwd42@gmail.com>
2017-12-09 18:00:14 -0500

    StockScraper1 initial commit

---
+++ StockScrapers/StockScraper1/StockScraper1/StockScraper1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StockScraper1.aspx.cs" Inherits="StockScraper1.StockScraper1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Stock Scraper</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="InputHereLabel" runat="server" Text="Input Here" /> 
+            <asp:TextBox ID="InputTextBox" runat="server" />   
+            <asp:Button ID="ClickMeButton" runat="server" Text="Click Me" OnClick="ClickMeButton_Click" /> <br /> 
+            <br /> <br /> 
+            <asp:Label ID="OutputLabel" runat="server" /> 
+        </div>
+    </form>
+</body>
+</html>
---
+++ StockScrapers/StockScraper1/StockScraper1/StockScraper1.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+
+// This project modeled after the tutorial on codeproject.com
+// .https://www.codeproject.com/Articles/659019/Scraping-HTML-DOM-elements-using-HtmlAgilityPack-H
+namespace StockScraper1
+{
+    public partial class StockScraper1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            var getHtmlWeb = new HtmlWeb();
+            var document = getHtmlWeb.Load(InputTextBox.Text);
+            var aTags = document.DocumentNode.SelectNodes("//a");
+            int counter = 1;
+            if (aTags != null)
+            {
+                foreach (var aTag in aTags)
+                {
+                    OutputLabel.Text += counter + ". " + aTag.InnerHtml + " - " + aTag.Attributes["href"].Value + "\t" + "<br />";
+                    counter++;
+                }
+            }
+        }
+    }
+}
