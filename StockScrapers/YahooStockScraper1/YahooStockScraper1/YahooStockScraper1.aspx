16ed0d3bfd83149f0ac8ddb64cc29876e0f9af1b
Edward Dupre <edwd42@gmail.com>
2017-12-10 12:45:51 -0500

    able to get historical data, now just needs to get cleaned up

--- StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx
+++ StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx
@@ -7,14 +7,24 @@
     <title></title>
 </head>
 <body>
+    <p>Yahoo Stock Scraper gets data from https://finance.yahoo.com/quote/<span>SYMBOL</span>/history?p=<span>SYMBOL</span>
+
+    </p>
     <form id="form1" runat="server">
         <div>
-            <asp:Label ID="InputHereLabel" runat="server" Text="Input Here" /> 
-            <asp:TextBox ID="InputTextBox" runat="server" />   
+            <asp:Label ID="InputHereLabel" runat="server" Text="Input Stock Symbol Here" /> 
+            <asp:TextBox ID="InputTextBox" value="hd" runat="server" />   
             <asp:Button ID="ClickMeButton" runat="server" Text="Click Me" OnClick="ClickMeButton_Click" /> <br /> 
             <br /> <br /> 
+            <asp:Label ID="RowCount" runat="server" />
             <asp:Label ID="OutputLabel" runat="server" /> 
         </div>
     </form>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(function () {
+            $('#ClickMeButton').click();
+        });
+    </script>
 </body>
 </html>
--- StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.cs
+++ StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.cs
@@ -8,6 +8,9 @@ using HtmlAgilityPack;
 
 // This project modeled largely after the tutorial on codeproject.com
 // .https://www.codeproject.com/Articles/659019/Scraping-HTML-DOM-elements-using-HtmlAgilityPack-H
+// but refactoring has changed it quite a lot
+// TODO: need to fix page keeps reloading and adding more and more data - it needs to stop after first pass
+
 
 namespace YahooStockScraper1
 {
@@ -20,22 +23,89 @@ namespace YahooStockScraper1
 
         protected void ClickMeButton_Click(object sender, EventArgs e)
         {
+            // Get stock symbol from user then get web page
+            var page = getYahooFinanceHistoricalData();
+
+            //getAtags(page);
+
+            // Get the data
+            getTDtags(page);
+
+        }
+        
+        public HtmlDocument getYahooFinanceHistoricalData()
+        {
             var getHtmlWeb = new HtmlWeb();
-            // .https://finance.yahoo.com/quote/MULE/history?p=MULE
-            // getHtmlWeb.Load("https://finance.yahoo.com/quote/");
             var document = "https://finance.yahoo.com/quote/";
             document += InputTextBox.Text;
-            document += "/history?p=";
-            document += InputTextBox.Text;
-            HtmlDocument page = getHtmlWeb.Load(document);
+                document += "/history?p=";
+                document += InputTextBox.Text;
+                HtmlDocument page = getHtmlWeb.Load(document);
 
+            return page;
+        }
+
+        public void getTDtags(HtmlDocument page)
+        {
+            /*
+             /html[@id='atomic']/body/div[@id='app']/div/div/div[@id='render-target-default']/div[@class='Bgc($bg-body) Mih(100%) W(100%) US']/div[@class='Pos(r) Bgc($bg-content) Miw(1007px) Maw(1260px) tablet_Miw(600px)--noRightRail Bxz(bb) Bdstartc(t) Bdstartw(20px) Bdendc(t) Bdends(s) Bdendw(20px) Bdstarts(s) Mx(a)']/div[@id='YDC-Col1']/div[@id='Main']/div[2]/div[@id='mrt-node-Col1-1-HistoricalDataTable']/div[@id='Col1-1-HistoricalDataTable-Proxy']/section[@class='smartphone_Px(20px)']/div[@class='Pb(10px) Ovx(a) W(100%)']/table[@class='W(100%) M(0)']/tbody/tr[@class='BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'][1]/td[@class='Py(10px) Ta(start) Pend(10px)']/span
+             */
+
+            //var trTags = page.DocumentNode.SelectNodes("//tr[" + row + "]");
+            var trRows = page.DocumentNode.SelectNodes("//tr");
+            //var tdTags = page.DocumentNode.SelectNodes("./span/text()");
+            int trCounter = 1;
+            if (trRows != null)
+            {
+                foreach (var trRow in trRows)
+                {
+                    trCounter++;
+                }
+                RowCount.Text = "Total rows: " + trCounter.ToString() + "<br /><br />";
+            }
+
+            for (var row = 0; row < trCounter; row++)
+            {
+                var trTags = page.DocumentNode.SelectNodes("//tr[" + row + "]");
+                if (trTags != null)
+                {
+                    foreach (var trTag in trTags)
+                    {
+                        //        trCounter++;
+                        //        // [@class='Py(10px) Pstart(10px)']
+                        //        // /html[@id='atomic']/body/div[@id='app']/div/div/div[@id='render-target-default']/div[@class='Bgc($bg-body) Mih(100%) W(100%) US']/div[@class='Pos(r) Bgc($bg-content) Miw(1007px) Maw(1260px) tablet_Miw(600px)--noRightRail Bxz(bb) Bdstartc(t) Bdstartw(20px) Bdendc(t) Bdends(s) Bdendw(20px) Bdstarts(s) Mx(a)']/div[@id='YDC-Col1']/div[@id='Main']/div[2]/div[@id='mrt-node-Col1-1-HistoricalDataTable']/div[@id='Col1-1-HistoricalDataTable-Proxy']/section[@class='smartphone_Px(20px)']/div[@class='Pb(10px) Ovx(a) W(100%)']/table[@class='W(100%) M(0)']/tbody/tr[@class='BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'][1]/td[@class='Py(10px) Pstart(10px)'][2]/span
+                        //           /html[@id='atomic']/body/div[@id='app']/div/div/div[@id='render-target-default']/div[@class='Bgc($bg-body) Mih(100%) W(100%) US']/div[@class='Pos(r) Bgc($bg-content) Miw(1007px) Maw(1260px) tablet_Miw(600px)--noRightRail Bxz(bb) Bdstartc(t) Bdstartw(20px) Bdendc(t) Bdends(s) Bdendw(20px) Bdstarts(s) Mx(a)']/div[@id='YDC-Col1']/div[@id='Main']/div[2]/div[@id='mrt-node-Col1-1-HistoricalDataTable']/div[@id='Col1-1-HistoricalDataTable-Proxy']/section[@class='smartphone_Px(20px)']/div[@class='Pb(10px) Ovx(a) W(100%)']/table[@class='W(100%) M(0)']/tbody/tr[@class='BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'][1]/td[@class='Py(10px) Pstart(10px)'][3]/span
+                        for (var col = 0; col <= 7; col++)
+                        {
+
+                            //var tdTags = page.DocumentNode.SelectNodes(@"/html[@id='atomic']/body/div[@id='app']/div/div/div[@id='render-target-default']/div[@class='Bgc($bg-body) Mih(100%) W(100%) US']/div[@class='Pos(r) Bgc($bg-content) Miw(1007px) Maw(1260px) tablet_Miw(600px)--noRightRail Bxz(bb) Bdstartc(t) Bdstartw(20px) Bdendc(t) Bdends(s) Bdendw(20px) Bdstarts(s) Mx(a)']/div[@id='YDC-Col1']/div[@id='Main']/div[2]/div[@id='mrt-node-Col1-1-HistoricalDataTable']/div[@id='Col1-1-HistoricalDataTable-Proxy']/section[@class='smartphone_Px(20px)']/div[@class='Pb(10px) Ovx(a) W(100%)']/table[@class='W(100%) M(0)']/tbody/tr[@class='BdT Bdc($c-fuji-grey-c) Ta(end) Fz(s) Whs(nw)'][1]/td[@class='Py(10px) Pstart(10px)'][" + col + "]/span");
+                            var tdTags = page.DocumentNode.SelectNodes("//tr[" + row + "]//td[" + col + "]/span");
+                            if (tdTags != null)
+                            {
+                                foreach (var tdTag in tdTags)
+                                {
+                                    OutputLabel.Text += tdTag.InnerHtml + " \t "; // + "\t" + "<br />";
+                                }
+                            }
+                            //OutputLabel.Text += "<br />";
+                        }
+                        OutputLabel.Text += trTag.Attributes["data-reactid"].Value + "<br />";
+                    }
+                }
+                //OutputLabel.Text += "<br />";
+            }
+        }
+
+
+        public void getAtags(HtmlDocument page)
+        {
             var aTags = page.DocumentNode.SelectNodes("//a");
             int counter = 1;
             if (aTags != null)
             {
                 foreach (var aTag in aTags)
                 {
-                    OutputLabel.Text += counter + ". " + aTag.InnerHtml + " - " + aTag.Attributes["href"].Value + "\t" + "<br />";
+                    OutputLabel.Text += counter + ". " + aTag.InnerHtml + " #-# " + aTag.Attributes["href"].Value + "\t" + "<br />";
                     counter++;
                 }
             }
