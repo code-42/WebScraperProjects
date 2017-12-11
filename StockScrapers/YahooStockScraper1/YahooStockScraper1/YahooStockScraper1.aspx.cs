3e5a65a20c447750035b91b49120334f649b24cc
Edward Dupre <edwd42@gmail.com>
2017-12-11 11:37:47 -0500

    add async method YahooScraper() but returns error about async handler

--- StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.cs
+++ StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Net;
-// using System.Net.Http;
+//using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using System.Text.RegularExpressions;
@@ -36,7 +38,9 @@ namespace YahooStockScraper1
             // var page = LoadPage2(stockSymbol)
 
             // Get stock symbol from user then get web page
-            GetYahooFinanceHistoricalData(page);
+            //GetYahooFinanceHistoricalData(page);
+
+            YahooScraper();
 
             //getAtags(page);
         }
@@ -87,25 +91,6 @@ namespace YahooStockScraper1
 
         public void GetYahooFinanceHistoricalData(HtmlDocument page)
         {
-             // Establish database connection
-            using (SqlConnection conn = new SqlConnection())
-            {
-                // Create connection string
-                conn.ConnectionString =
-                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MeAdmin\Documents\YahooData.mdf;Integrated Security=True;Connect Timeout=30";
-
-                try
-                {
-                    conn.Open();
-                    DataBaseLabel.Text = "Database connection opened!" + "<br /><br />";
-                }
-                catch (Exception e)
-                {
-                    DataBaseLabel.Text = "My Bad: " + e + "<br /><br />";
-                }
-            }
-
-
             // Count the number of rows to retrieve
             var trRows = page.DocumentNode.SelectNodes("//tr");
             int trCounter = 1;
@@ -118,38 +103,98 @@ namespace YahooStockScraper1
                 RowCount.Text = "Total rows: " + trCounter.ToString() + "<br /><br />";
             }
 
-            // Loop through the data rows
-            for (var row = 0; row < trCounter; row++)
+            //// Loop through the data rows
+            //for (var row = 0; row < trCounter; row++)
+            //{
+            //    // Select all tr that do not have the specified class in their descendants .//
+            //    var trTags =
+            //        page.DocumentNode.SelectNodes("//tr[" + row +
+            //                                      "][not(.//@class='Ta(c) Py(10px) Pstart(10px)')]");
+            //    if (trTags != null)
+            //    {
+            //        foreach (var trTag in trTags)
+            //        {
+            //            // Loop through the columns to select the data
+            //            for (var col = 0; col <= 7; col++)
+            //            {
+            //                // Select the data in the col
+            //                var selectNodes = "//tbody//tr[" + row + "]//td[" + col + "]/span";
+            //                var tdTags = page.DocumentNode.SelectNodes(selectNodes);
+            //                if (tdTags != null)
+            //                {
+            //                    foreach (var tdTag in tdTags)
+            //                    {
+            //                        {
+            //                            OutputLabel.Text += tdTag.InnerText + " \t "; // + "\t" + "<br />";
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //            // Print a line break after each tr
+            //            OutputLabel.Text += "<br />";
+            //        }
+            //    }
+            //}
+        }
+
+
+        public static async void YahooScraper()
+        {
+            // Establish database connection
+            using (SqlConnection conn = new SqlConnection())
             {
-                // Select all tr that do not have the specified class in their descendants .//
-                var trTags = page.DocumentNode.SelectNodes("//tr[" + row + "][not(.//@class='Ta(c) Py(10px) Pstart(10px)')]");
-                if (trTags != null)
+                // Create connection string
+                conn.ConnectionString =
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MeAdmin\Documents\YahooData.mdf;Integrated Security=True;Connect Timeout=30"
+                    ;
+
+                try
                 {
-                    foreach (var trTag in trTags)
-                    {
-                        // Loop through the columns to select the data
-                        for (var col = 0; col <= 7; col++)
-                        {
-                            // Select the data in the col
-                            var selectNodes = "//tbody//tr[" + row + "]//td[" + col + "]/span";
-                            var tdTags = page.DocumentNode.SelectNodes(selectNodes);
-                            if (tdTags != null)
-                            {
-                                foreach (var tdTag in tdTags)
-                                {
-                                    {
-                                        OutputLabel.Text += tdTag.InnerText + " \t "; // + "\t" + "<br />";
-                                    }
-                                }
-                            }
-                        }
-                        // Print a line break after each tr
-                        OutputLabel.Text += "<br />";
-                    }
+                    conn.Open();
+                    //DataBaseLabel.Text = "Database connection opened!" + "<br /><br />";
+                }
+                catch (Exception e)
+                {
+                    //DataBaseLabel.Text = "My Bad: " + e + "<br /><br />";
+                }
+
+                SqlCommand insertCommand = new SqlCommand(
+                    "INSERT INTO SQLCodeProject (Date, Open, High, Low, Close, AdjClose, Volume) VALUES (@0, @1, @2, @3, @4, @5, @6)",
+                    conn);
+
+                var url = "https://finance.yahoo.com/quote/HD/history?p=HD";
+
+                // This method call using System.Net.Http;
+                // Requires method to be async
+                // Need to add await to method call
+                //var httpclient = new HttpClient();
+                //var html = await httpclient.GetStringAsync(url);
+
+                var getHtmlWeb = new HtmlWeb();
+                HtmlDocument page = getHtmlWeb.Load(url);
+
+                // This call using HtmlAgilityPack package
+                var htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(url);
+
+                // Scraping modeled after example from .https://youtu.be/B4x4pnLYMWI
+                // Use HtmlAgilityPack to parse out data
+                var tr = page.DocumentNode.Descendants("tr")
+                    .Where(trNode => trNode.GetAttributeValue("class", "")
+                        .Contains("BdT Bdc($c-fuji-grey-c)")).ToList();
+
+                var td = page.DocumentNode.Descendants("td")
+                    .Where(tdNode => tdNode.HasAttributes.Equals("data-reactid")).ToList();
+
+                foreach (var span in td)
+                {
+                    // Date
+                    var yDate = (span.GetAttributeValue("data-reactid", ""));
+
+                    //DataBuider.Text += "test" + yDate;
                 }
             }
         }
-
 
         public void getAtags(HtmlDocument page)
         {
--- StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.designer.cs
+++ StockScrapers/YahooStockScraper1/YahooStockScraper1/YahooStockScraper1.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace YahooStockScraper1 {
         protected global::System.Web.UI.WebControls.Label DataBaseLabel;
         
         /// <summary>
+        /// DataBuider control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label DataBuider;
+        
+        /// <summary>
         /// RowCount control.
         /// </summary>
         /// <remarks>
