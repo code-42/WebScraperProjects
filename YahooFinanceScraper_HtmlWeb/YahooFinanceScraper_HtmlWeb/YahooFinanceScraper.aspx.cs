59c93b0c432413f2f26c227dabf15fbe47885c55
Edward Dupre <edwd42@gmail.com>
2017-12-17 08:49:15 -0500

    cleaned up YahooFinanceScraper_HtmlWeb superfluous code and comments

--- YahooFinanceScraper_HtmlWeb/YahooFinanceScraper_HtmlWeb/YahooFinanceScraper.aspx.cs
+++ YahooFinanceScraper_HtmlWeb/YahooFinanceScraper_HtmlWeb/YahooFinanceScraper.aspx.cs
@@ -34,15 +34,10 @@ namespace YahooFinanceScraper
             // Get stock symbol from user
             var stockSymbol = getStocksymbol();
 
-            var page = LoadPage1(stockSymbol);
-            // var page = LoadPage2(stockSymbol)
-
+            var page = LoadPage(stockSymbol);
+            
             // Get stock symbol from user then get web page
             GetYahooFinanceHistoricalData(page);
-
-            //YahooScraper();
-
-            //getAtags(page);
         }
 
         public string getStocksymbol()
@@ -50,7 +45,7 @@ namespace YahooFinanceScraper
             return InputTextBox.Text;
         }
 
-        public HtmlDocument LoadPage1(string stockSymbol)
+        public HtmlDocument LoadPage(string stockSymbol)
         {
             // There are several ways to load a web page - heres one
             // .https://www.codeproject.com/Articles/659019/Scraping-HTML-DOM-elements-using-HtmlAgilityPack-H
@@ -65,28 +60,6 @@ namespace YahooFinanceScraper
             HtmlDocument page = getHtmlWeb.Load(document);
 
             return page;
-        }
-
-        public HtmlDocument LoadPage2(string stockSymbol)
-        {
-            // There are several ways to load a web page - heres another
-            // .http://blog.adrian-thomas.com/2013/02/starters-guide-to-web-scraping-with.html
-
-            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
-            var document = "https://finance.yahoo.com/quote/";
-            document += stockSymbol;
-            document += "/history?p=";
-            document += stockSymbol;
-
-            using (var webClient = new System.Net.WebClient())
-            {
-                using (var stream = webClient.OpenRead(document))
-                {
-                    htmlDocument.Load(stream);
-                }
-            }
-
-            return htmlDocument;
         }
 
         public void GetYahooFinanceHistoricalData(HtmlDocument page)
@@ -160,7 +133,7 @@ namespace YahooFinanceScraper
                                 {
                                     foreach (var tdTag in tdTags)
                                     {
-                                        //OutputLabel.Text += tdTag.InnerText + " \t ";
+                                        OutputLabel.Text += tdTag.InnerText + " \t ";
                                         //Array.add(dataArr, tdTag.InnerText);
                                         dataArr[col] = tdTag.InnerText;
                                     }
@@ -172,11 +145,11 @@ namespace YahooFinanceScraper
                                 {
                                     case 1:
                                         Date = Convert.ToDateTime(dataArr[col]);
-                                        DataArrLabel1.Text += dataArr[col] + " \t ";
+                                        // DataArrLabel1.Text += dataArr[col] + " \t ";
                                         break;
                                     case 2:
                                         Open = float.Parse(dataArr[col]);
-                                        DataArrLabel1.Text += dataArr[col] + " \t ";
+                                        // DataArrLabel1.Text += dataArr[col] + " \t ";
                                         break;
                                     case 3:
                                         High = float.Parse(dataArr[col]);
@@ -225,7 +198,7 @@ namespace YahooFinanceScraper
 
 
                             // Print a line break after each tr
-                            //OutputLabel.Text += "<br />";
+                            OutputLabel.Text += "<br />";
                             //DataArrLabel1.Text += "<br />";
 
                             //foreach (string item in data)
@@ -278,78 +251,5 @@ namespace YahooFinanceScraper
             }
         }
 
-
-        public static void YahooScraper()
-        {
-            // Establish database connection
-            using (SqlConnection conn = new SqlConnection())
-            {
-                // Create connection string
-                conn.ConnectionString =
-                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\MeAdmin\Documents\YahooData.mdf;Integrated Security=True;Connect Timeout=30"
-                    ;
-
-                try
-                {
-                    conn.Open();
-                    //DataBaseLabel.Text = "Database connection opened!" + "<br /><br />";
-                }
-                catch (Exception e)
-                {
-                    //DataBaseLabel.Text = "My Bad: " + e + "<br /><br />";
-                }
-
-                SqlCommand insertCommand = new SqlCommand(
-                    "INSERT INTO SQLCodeProject (Date, Open, High, Low, Close, AdjClose, Volume) VALUES (@0, @1, @2, @3, @4, @5, @6)",
-                    conn);
-
-                var url = "https://finance.yahoo.com/quote/HD/history?p=HD";
-
-                // This method call using System.Net.Http;
-                // Requires method to be async
-                // Need to add await to method call
-                //var httpclient = new HttpClient();
-                //var html = await httpclient.GetStringAsync(url);
-
-                var getHtmlWeb = new HtmlWeb();
-                HtmlDocument page = getHtmlWeb.Load(url);
-
-                // This call using HtmlAgilityPack package
-                var htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(url);
-
-                // Scraping modeled after example from .https://youtu.be/B4x4pnLYMWI
-                // Use HtmlAgilityPack to parse out data
-                var tr = page.DocumentNode.Descendants("tr")
-                    .Where(trNode => trNode.GetAttributeValue("class", "")
-                        .Contains("BdT Bdc($c-fuji-grey-c)")).ToList();
-
-                var td = page.DocumentNode.Descendants("td")
-                    .Where(tdNode => tdNode.HasAttributes.Equals("data-reactid")).ToList();
-
-                foreach (var span in td)
-                {
-                    // Date
-                    var yDate = (span.GetAttributeValue("data-reactid", ""));
-
-
-                    //DataBuiderLabel.Text += "test" + yDate;
-                }
-            }
-        }
-
-        public void getAtags(HtmlDocument page)
-        {
-            var aTags = page.DocumentNode.SelectNodes("//a");
-            int counter = 1;
-            if (aTags != null)
-            {
-                foreach (var aTag in aTags)
-                {
-                    OutputLabel.Text += counter + ". " + aTag.InnerHtml + " #-# " + aTag.Attributes["href"].Value + "\t" + "<br />";
-                    counter++;
-                }
-            }
-        }
     }
 }
